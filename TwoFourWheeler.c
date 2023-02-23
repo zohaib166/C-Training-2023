129d68ae8a1f3ac0a3ae4f09d78a73b3ee468a2f
zohaib166 <92329661+zohaib166@users.noreply.github.com>
2023-02-24 00:21:07 +0530

    Create TwoFourWheeler.c

---
+++ TwoFourWheeler.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int two_wheelers, four_wheelers;
+    int total_vehicles, total_wheels;
+    int t = 5;
+    while(t--) {
+        scanf("%d %d", &total_vehicles, &total_wheels); //do not change this line
+
+        //Write your code here
+
+        printf("%d %d\n", two_wheelers, four_wheelers); //do not change this line
+    }  
+}
