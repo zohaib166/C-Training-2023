39f6adc0cef82195f32f196365fca73f2d3273db
zohaib166 <92329661+zohaib166@users.noreply.github.com>
2023-02-24 07:34:53 +0530

    Update TwoFourWheeler.c

--- TwoFourWheeler.c
+++ TwoFourWheeler.c
@@ -2,12 +2,11 @@
 int main() {
     int two_wheelers, four_wheelers;
     int total_vehicles, total_wheels;
-    int t = 5;
-    while(t--) {
-        scanf("%d %d", &total_vehicles, &total_wheels); //do not change this line
+    
+    scanf("%d %d", &total_vehicles, &total_wheels); //do not change this line
 
-        //Write your code here
+    //Write your code here
 
-        printf("%d %d\n", two_wheelers, four_wheelers); //do not change this line
-    }  
+    printf("%d %d\n", two_wheelers, four_wheelers); //do not change this line
+  
 }
